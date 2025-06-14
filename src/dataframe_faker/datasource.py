9fa7c631805d787850994a84de613267942bce53
Ville Puuska <puuskaville@gmail.com>
2025-06-14 14:01:32 +0300

    initial implementation of DataframeFakerStreamReader

--- src/dataframe_faker/datasource.py
+++ src/dataframe_faker/datasource.py
@@ -1,6 +1,12 @@
-from typing import TYPE_CHECKING, Iterator, Sequence, final
+import json
+from typing import TYPE_CHECKING, Any, Iterator, Sequence, final
 
+from faker import Faker
+from pyspark.sql import Row
 from pyspark.sql.types import StructType
+
+from .constraints import Constraint
+from .dataframe import _generate_fake_rows
 
 DATASOURCE_API_SUPPORTED = True
 
@@ -42,20 +48,45 @@ else:
 
 @final
 class DataframeFakerStreamReader(SimpleDataSourceStreamReader):
-    def __init__(self, options: dict) -> None:
-        raise NotImplementedError
+    def __init__(
+        self,
+        schema: StructType,
+        constraints: dict[str, Constraint | dict[str, Any] | None] | None = None,
+        rows: int = 5,
+        fake: Faker | None = None,
+    ) -> None:
+        self.schema = schema
+        self.constraints = constraints or {}
+        self.rows = rows
+        self.fake = fake or Faker()
 
     def initialOffset(self) -> dict:
-        raise NotImplementedError
+        return {"offset": 0}
 
     def read(self, start: dict) -> tuple[Iterator[tuple], dict]:
-        raise NotImplementedError
+        data = _generate_fake_rows(
+            schema=self.schema,
+            constraints=self.constraints,
+            rows=self.rows,
+            fake=self.fake,
+        )
+        # Returning a `Row` is actually allowed, but the PySpark API typing is 'incorrect'.
+        return iter([Row(**row) for row in data]), {
+            "offset": start.get("offset", 0) + self.rows
+        }  # pyrefly: ignore
 
     def readBetweenOffsets(self, start: dict, end: dict) -> Iterator[tuple]:
-        raise NotImplementedError
+        data = _generate_fake_rows(
+            schema=self.schema,
+            constraints=self.constraints,
+            rows=end.get("offset", 0) - start.get("offset", 0) + 1,
+            fake=self.fake,
+        )
+        # Returning a `Row` is actually allowed, but the PySpark API typing is 'incorrect'.
+        return iter([Row(**row) for row in data])  # pyrefly: ignore
 
     def commit(self, end: dict) -> None:
-        raise NotImplementedError
+        pass
 
 
 @final
@@ -71,15 +102,27 @@ class DataframeFakerReader(DataSourceReader):
 
 @final
 class DataframeFaker(DataSource):
+    @classmethod
+    def name(cls) -> str:
+        """The name of the datasource."""
+        return "dataframe-faker"
+
     def __init__(self, options: dict[str, str]) -> None:
         super().__init__(options)
-        raise NotImplementedError
+        self.options = options
 
     def schema(self) -> StructType | str:
+        """
+        Raises a value error
+        """
         raise NotImplementedError
 
     def simpleStreamReader(self, schema: StructType) -> SimpleDataSourceStreamReader:
-        raise NotImplementedError
+        return DataframeFakerStreamReader(
+            schema=schema,
+            constraints=json.loads(self.options.get("constraints", "{}")),
+            rows=int(self.options.get("rows", 100)),
+        )
 
     def reader(self, schema: StructType) -> DataSourceReader:
         raise NotImplementedError
